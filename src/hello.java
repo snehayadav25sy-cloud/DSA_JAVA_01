fc933a9605a06b048bf521dd556aa5911f6dfbee
Sneha Yadav <snehayadav25@gmail.com>
2026-02-04 19:23:09 +0530

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public void Hello(){
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        hello h = new hello();
+        h.Hello();
+    }
+}
