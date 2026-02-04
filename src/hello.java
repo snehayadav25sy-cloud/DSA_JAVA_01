b2d371bd5aea560d63de3f2ea6fb846bb21cc68c
Sneha Yadav <snehayadav25@gmail.com>
2026-02-04 20:56:00 +0530

    test01

--- src/hello.java
+++ src/hello.java
@@ -4,7 +4,7 @@ public class hello {
     }
 
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome! to github");
         hello h = new hello();
         h.Hello();
     }
